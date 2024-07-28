43bd48279cfaafbe6c3fe10744c6f8e111eeacf6
SeRGe <sergiosamo@protonmail.com>
2024-07-28 15:02:56 +0200

    flowershop finito2

--- src/main/java/flowershop/application/Flowershop.java
+++ src/main/java/flowershop/application/Flowershop.java
@@ -3,13 +3,20 @@ package flowershop.application;
 import flowershop.configdb.mysql.CreateDatabaseMysql;
 import flowershop.dao.DaoManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Flowershop {
     private String database;
     static Flowershop instance;
     private DaoManager manager;
+    private static final Logger logger = Logger.getLogger(Flowershop.class.getName());
 
     private Flowershop() {
-        database = "mysql";
+        database = loadDatabaseConfig();
     }
 
     private static void setInstance() {
@@ -20,7 +27,7 @@ public class Flowershop {
 
     public static void start() {
         Flowershop.setInstance();
-        CreateDatabaseMysql.initializeDatabase();
+        instance.loadDatabase(instance.database);
         instance.setDaoManager();
         Menu menu = new Menu(instance.getManager());
         menu.startMenu();
@@ -33,5 +40,28 @@ public class Flowershop {
 
     public DaoManager getManager() {
         return manager;
+    }
+
+    private void loadDatabase(String database) {
+        if ("mysql".equalsIgnoreCase(database)) {
+            CreateDatabaseMysql.initializeDatabase();
+        } else if ("mongo".equalsIgnoreCase(database)) {
+            //.getMondoDatabase
+        }
+    }
+
+    private String loadDatabaseConfig() {
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                logger.log(Level.SEVERE, "Archivo config.properties no encontrado");
+                return "mysql";
+            }
+            properties.load(input);
+            return properties.getProperty("database", "mysql");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error al cargar la configuración de la base de datos", e);
+            return "mysql";
+        }
     }
 }
