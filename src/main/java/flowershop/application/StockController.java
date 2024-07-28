234b2086721c31ed8691b36aced674f9d9a9a8d7
K1-PC2 <dav.cuadrado@gmail.com>
2024-07-28 23:57:59 +0200

    fixed constructor and method references

--- src/main/java/flowershop/application/StockController.java
+++ src/main/java/flowershop/application/StockController.java
@@ -6,7 +6,6 @@ import flowershop.helpers.Input;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
