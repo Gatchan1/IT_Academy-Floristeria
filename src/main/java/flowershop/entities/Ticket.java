e9960b96e6111f0a3e8fe6e240c68da039d4a3d8
K1-PC2 <dav.cuadrado@gmail.com>
2024-07-28 10:24:04 +0200

    added equals and hashCode methods. Format update

--- src/main/java/flowershop/entities/Ticket.java
+++ src/main/java/flowershop/entities/Ticket.java
@@ -11,40 +11,45 @@ public class Ticket {
     private double saleTotal;
 
     public Ticket(){
-        this.saleDate = LocalDate.now();
     }
 
     public Ticket(HashMap<Product, Integer> saleProducts, double saleTotal){
         this.saleProducts = saleProducts;
         this.saleTotal = saleTotal;
-        this.saleDate = LocalDate.now();
     }
 
     public int getIdTicket() {
+
         return idTicket;
     }
 
     public void setIdTicket(int idTicket) {
+
         this.idTicket = idTicket;
     }
 
     public HashMap<Product, Integer> getSaleProducts() {
+
         return saleProducts;
     }
 
     public void setSaleProducts(HashMap<Product, Integer> saleProducts) {
+
         this.saleProducts = saleProducts;
     }
 
     public double getSaleTotal() {
+
         return saleTotal;
     }
 
     public void setSaleTotal(double saleTotal) {
+
         this.saleTotal = saleTotal;
     }
 
     public LocalDate getSaleDate() {
+
         return saleDate;
     }
 
@@ -52,5 +57,17 @@ public class Ticket {
         this.saleDate = LocalDate.now;
     }
 
-    //hash&equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return Double.compare(ticket.saleTotal, saleTotal) == 0 &&
+                Objects.equals(saleProducts, ticket.saleProducts) &&
+                Objects.equals(saleDate, ticket.saleDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(saleProducts, saleDate, saleTotal);
 }
