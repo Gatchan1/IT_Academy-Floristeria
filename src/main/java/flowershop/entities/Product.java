ca03f32a00ce026fc34cb47b0264b34fd9267bbf
Gatchan1 <r.barrio.f@gmail.com>
2024-07-29 18:55:07 +0200

    created alternate toString method

--- src/main/java/flowershop/entities/Product.java
+++ src/main/java/flowershop/entities/Product.java
@@ -1,18 +1,12 @@
 package flowershop.entities;
 
-import java.util.Objects;
-
-public class Product{
-
+public abstract class Product {
     protected String id;
     protected String name;
     protected double price;
     protected int stock;
 
-    public Product(){
-    }
-
-    public Product(String name, double price, int stock){
+    public Product(String name, double price, int stock) {
         this.name = name;
         this.price = price;
         this.stock = stock;
@@ -31,44 +25,24 @@ public class Product{
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 
     public double getPrice() {
-
         return price;
     }
 
     public void setPrice(double price) {
-
         this.price = price;
     }
 
     public int getStock() {
-
         return stock;
     }
 
     public void setStock(int stock) {
-
         this.stock = stock;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-           return true;
-        }
-        if (o == null || getClass() != o.getClass()){
-           return  false;
-        }
-        Product product = (Product) o;
-        return Objects.equals(name, product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
+    public abstract String toStringAlt();
 }
