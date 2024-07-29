21f9f6e7703aeae3036c494454e51dc2f6ee94f0
SeRGe <sergiosamo@protonmail.com>
2024-07-29 12:14:25 +0200

    modification menu

--- src/main/java/flowershop/application/Menu.java
+++ src/main/java/flowershop/application/Menu.java
@@ -83,6 +83,7 @@ public class Menu {
         } while (!exit);
     }
 
+
     public static void byeBye() {
         System.out.println("\n" +
                 "***************************************\n" +
